a47adeb2eec35b861dd7adcda3dc5a5f5fab8f69
YOOH HAN HWANG <103127767+rokmc1256@users.noreply.github.com>
2024-01-11 06:27:32 +0900

    Time: 2178 ms (37.67%), Space: 14.6 MB (6.53%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        output = []
+        for i in range(len(nums)):
+            i_val = nums[i]
+            for j in range(i+1, len(nums)):
+                j_val = nums[j]
+                if i_val + j_val == target:
+                    output.append(i)
+                    output.append(j)
+                    return output
